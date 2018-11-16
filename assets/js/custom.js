ce6d0ddfc3accc834c2dc3d5c75fd847809a13fc
Amirreza <amirreza.developer@gmail.com>
2018-11-16 11:58:54 +0330

    Add collapse mechanism to panel widget

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,3 +6,12 @@ if ($('.flash-message-container').length > 0) {
         }, (index * 4000) + 4000);
     });
 }
+
+$('.collapse-link').on('click', function () {
+    var panel = $(this).closest('.panel'),
+        icon = $(this).find('i'),
+        content = panel.find('.panel-body');
+    content.slideToggle(200);
+    content.css('height', 'auto');
+    icon.toggleClass('fa-angle-up fa-angle-down');
+});
