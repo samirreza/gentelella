a118e3d79dd5b5a9ded2fdc26731542c4b90a284
Amirreza Yeganegi <amirreza.developer@gmail.com>
2018-12-23 19:22:57 +0330

    Convert count to farsi number in InfoBox widget

--- widgets/infoBox/views/box.php
+++ widgets/infoBox/views/box.php
@@ -1,7 +1,7 @@
 <div class="tile-stats">
     <div class="icon">
-        <i class="fa fa-<?=$icon?>"></i>
+        <i class="fa fa-<?= $icon ?>"></i>
     </div>
-    <div class="count"><?=$count?></div>
-    <h4><?=$title?></h4>
+    <div class="count"><?= Yii::$app->formatter->asFarsiNumber($count) ?></div>
+    <h4><?= $title ?></h4>
 </div>
