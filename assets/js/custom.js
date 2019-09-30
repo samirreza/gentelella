76fab7d4e3e0614507d94a53c60e36e0e68b0bb8
faridplus <faridplusplus@yahoo.com>
2019-09-30 23:07:19 +0330

    interface stuff

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -121,3 +121,17 @@ function toggleFullscreen() {
         }
     }
 }
+
+
+function getUrlVars()
+{
+    let vars = [], hash;
+    let hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(let i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
